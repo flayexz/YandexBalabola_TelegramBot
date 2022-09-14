b8f88a5458142204b39c6a037d7e044d89a4156c
flayexz <hxjsnsjdkswb@mail.ru>
2022-09-14 19:21:15 +0300

    [add] bot class

---
+++ BalabolaBot/BalabolaBot.Core/Classes/Bot.cs
@@ -0,0 +1,19 @@
+﻿using BalabolaBot.Core.Commands;
+using Telegram.Bot;
+
+namespace BalabolaBot.Core.Classes;
+
+public class Bot
+{
+    public IReadOnlyCollection<Command> Commands => commands.AsReadOnly();
+    private readonly List<Command> commands;
+
+    public readonly TelegramBotClient Client;
+    
+    
+    public Bot(string token)
+    {
+        Client = new TelegramBotClient(token);
+        commands = ReflectiveEnumerator.GetEnumerableOfType<Command>().ToList();
+    }
+}
---
+++ BalabolaBot/BalabolaBot.Core/Classes/ReflectiveEnumerator.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+
+namespace BalabolaBot.Core.Classes;
+
+public static class ReflectiveEnumerator
+{
+    static ReflectiveEnumerator() { }
+
+    public static IEnumerable<T> GetEnumerableOfType<T>(params object[] constructorArgs) where T : class
+    {
+        var objects = Assembly.GetAssembly(typeof(T))
+            .GetTypes()
+            .Where(myType => myType.IsClass && !myType.IsAbstract && myType.IsSubclassOf(typeof(T)))
+            .Select(type => (T)Activator.CreateInstance(type, constructorArgs))
+            .ToList();
+        return objects;
+    }
+}
