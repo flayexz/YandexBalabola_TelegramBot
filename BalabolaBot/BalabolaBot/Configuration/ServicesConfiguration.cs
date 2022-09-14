fb1e5c3b4e1bf076a9f39bedd4c0d876bd837a0e
flayexz <hxjsnsjdkswb@mail.ru>
2022-09-14 20:18:40 +0300

    [add] message controller for updates

---
+++ BalabolaBot/BalabolaBot/Configuration/ServicesConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using BalabolaBot.Core.Classes;
+
+namespace BalabolaBot.Configuration;
+
+public static class ServicesConfiguration
+{
+    public static IServiceCollection ConfigureServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddSingleton(new Bot(configuration.GetRequiredSection("Key").ToString()!));
+        return services;
+    }
+}
---
+++ BalabolaBot/BalabolaBot/Controllers/MessageController.cs
@@ -0,0 +1,36 @@
+﻿using BalabolaBot.Core.Classes;
+using Microsoft.AspNetCore.Mvc;
+using Telegram.Bot.Types;
+
+namespace BalabolaBot.Controllers;
+
+[ApiController]
+public class MessageController : Controller
+{
+    private readonly Bot bot;
+
+    public MessageController(Bot bot)
+    {
+        this.bot = bot;
+    }
+
+    [Route(@"api/message/update")]
+    public OkResult Update(Update update)
+    {
+        var commands = bot.Commands;
+        var message = update.Message;
+        if (message is null) return Ok();
+        var client = bot.Client;
+        var text = message.Text;
+        if (string.IsNullOrEmpty(text)) return Ok();
+        
+        foreach (var command in commands)
+        {
+            if (!command.Contains(text)) continue;
+            command.Execute(message, client);
+            break;
+        }
+
+        return Ok();
+    }
+}
