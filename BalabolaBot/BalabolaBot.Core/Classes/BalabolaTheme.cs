02cbc7fb43cb0d9ceeeceacdab690510a845ee24
flayexz <hxjsnsjdkswb@mail.ru>
2022-09-15 15:46:10 +0300

    [add] balabola service interface

---
+++ BalabolaBot/BalabolaBot.Core/Classes/BalabolaTheme.cs
@@ -0,0 +1,12 @@
+﻿namespace BalabolaBot.Core.Classes;
+
+public enum BalabolaTheme : int
+{
+    Default = 0,
+    ShortHistory = 6,
+    Wikipedia = 8, 
+    MovieSynopses = 9,
+    FolkWisdom = 11,
+    Instruction = 24,
+    Recipe = 25,
+}
---
+++ BalabolaBot/BalabolaBota.Infrastrucute/Services/BalabolaService.cs
@@ -0,0 +1,12 @@
+﻿using BalabolaBot.Core.Classes;
+using BalabolaBot.Core.Interfaces;
+
+namespace BalabolaBot.Infrastrucute.Services;
+
+public class BalabolaService : IBalabolaService
+{
+    public Task<string> GetBalabolaAsync(string message, BalabolaTheme theme = BalabolaTheme.Default)
+    {
+        throw new NotImplementedException();
+    }
+}
